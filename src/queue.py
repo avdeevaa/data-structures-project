50e857972b1aa692714f95981251b841f70eff81
avdeevaa <tess-avdeeva@seznam.cz>
2023-09-12 14:23:39 +0200

    third commit + push: method __str__ in Stack + tests class Node and Queue and tests

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,13 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        new_node = Node(data, None)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def dequeue(self):
         """
@@ -35,4 +43,10 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next_node
+
+        return "\n".join(result)
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+
+from src.queue import Queue
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
+
+    def test_enqueue(self):
+        self.queue.enqueue('data1')
+        self.assertNotEqual(self.queue.enqueue('data1'), 'data1')
+        self.assertEqual(self.queue.enqueue('data1'), None)
+
+    def test_str(self):
+        self.assertEqual(self.queue.__str__(), "")
+
+
+if __name__ == '__main__':
+    unittest.main()
