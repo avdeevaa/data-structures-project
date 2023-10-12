4dc95da1d84b240395f64aac4960dbf4b6a43264
avdeevaa <tess-avdeeva@seznam.cz>
2023-10-12 09:58:21 +0200

    5th commit + push: class LinkedList tests for class LinkedList

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.linked_list = LinkedList()
+
+    def test_insert_beginning(self):
+        self.linked_list.insert_beginning({'id': 1})
+        self.assertNotEqual(self.linked_list.insert_beginning({'id': 1}), {'id': 2})
+        self.assertEqual(self.linked_list.insert_beginning({'id': 1}), None)
+
+    def test_insert_at_end(self):
+        self.linked_list.insert_at_end({'id': 2})
+        self.assertEqual(self.linked_list.insert_at_end({'id': 2}), None)
+        self.assertNotEqual(self.linked_list.insert_at_end({'id': 2}), {'id': 1})
+
+    def test_str(self):
+        self.assertEqual(self.linked_list.__str__(), "None")
